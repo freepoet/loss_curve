69678246ffd2fbcc2d90de492f7863e61577e680
freepoet <ningmq_cv@foxmai.com>
2020-11-09 20:48:10 +0800

    fixed some bugs

--- d2_loss_visualization.py
+++ d2_loss_visualization.py
@@ -17,24 +17,33 @@ y1 = fig.add_subplot(111)
 y1.set_xlabel('Iterations')
 y2 = y1.twinx()
 y1.set_ylim(0,1.0)
+parsed=[]
 with open('./metrics.json') as f:
-    whole = f.read()
-    #pattern = re.compile(r'json_stats: (\{.*\})')
-    pattern = re.compile(r'\"data_time\":.*')
-    #r:ban zhaunyi
-    lis = pattern.findall(whole)
-    # try:
-    parsed = [json.loads(j) for j in lis]
-    print(parsed[0])
-    # except:
-    #     print("json format is not corrrect")
-    #     exit(1)
+    # whole = f.read()
+    # #pattern = re.compile(r'json_stats: (\{.*\})')
+    # pattern = re.compile(r'\"data_time\":.*')
+    # #r:ban zhaunyi
+    # lis = pattern.findall(whole)
+    try:
+        #ke neng chu xian de yi chang kuai
+        # parsed = [json.loads(j) for j in lis]
+        for line in f:
+            parsed.append(json.loads(line))
+        print(parsed[0])
 
-    _iter = [ j['iter'] for j in parsed]
-    _loss = [ j['loss'] for j in parsed]
-    _loss_bbox = [ j['loss_bbox'] for j in parsed]
+    except:
+        #shang shu yi chang kuai de chu li fang fa
+        print("json format is not corrrect")
+        exit(1)
+
+    _iter = [ j['iteration'] for j in parsed]
+    _loss = [ j['total_loss'] for j in parsed]
+    _loss_bbox = [ j['loss_box_reg'] for j in parsed]
     _loss_cls = [ j['loss_cls'] for j in parsed]
-    _accuracy_cls = [ j['accuracy_cls'] for j in parsed]
+    try:
+         _accuracy_cls = [ j['cls_accuracy'] for j in parsed]
+    except:
+        _accuracy_cls = None
     _lr = [ j['lr'] for j in parsed]
     try:
         _mask_loss = [ j['mask_loss'] for j in parsed]
@@ -44,9 +53,9 @@ with open('./metrics.json') as f:
     y1.plot(_iter, _loss_bbox, color="green", linewidth=1.0,linestyle="-",label='loss_box_reg')
     y1.plot(_iter, _loss, color="blue", linewidth=1.0, linestyle="-",label='total_loss')
     y1.plot(_iter, _loss_cls, color="black", linewidth=1.0, linestyle="-",label='loss_cls')
-    # y1.plot(_iter, _accuracy_cls, color="red", linewidth=1.0, linestyle="-",label='accuracy_cls')
-    # if _mask_loss is not None:
-    #     y1.plot(_iter, _mask_loss, color="grey", linewidth=1.0, linestyle="-",label='mask_loss')
+    y1.plot(_iter, _accuracy_cls, color="red", linewidth=1.0, linestyle="-",label='cls_accuracy')
+    if _mask_loss is not None:
+         y1.plot(_iter, _mask_loss, color="grey", linewidth=1.0, linestyle="-",label='mask_loss')
 
     y2.set_ylim(0,max(_lr)/0.8)
     y2.plot(_iter, _lr, color="purple", linewidth=1.0, linestyle="-",label='lr')
